171befd09f21b2c3a1990b7e6550f3d64d6d7862
BorisVV <bcarlof@gmail.com>
2018-10-08 18:30:51 -0500

    changed the Driver to add jc

--- src/com/BorisV/java/Record_Store_Data_Base.java
+++ src/com/BorisV/java/Record_Store_Data_Base.java
@@ -4,13 +4,13 @@ import java.sql.*;
 
 public class Record_Store_Data_Base extends Consignors_Model{
 
-    private static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; //New Driver add jc. Old was com.mysql.jdbc.Driver.
     private static String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/";
-    private static final String USER = "root";
-    private static final String PASS = "primero13";
+    private static final String USER = "user_records";
+    private static final String PASS = "user_records";
 
 
-    private static final String DB_NAME = "recordStoreData";
+    private static final String DB_NAME = "record_store_db";
 
     protected final static String RECSTORE_DATA_TABLE = "albums";
 
